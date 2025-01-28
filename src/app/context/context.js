a7477fc3227e65017186f9b065734930833dedd9
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2025-01-28 22:13:27 +0200

    first commit

--- src/app/context/context.js
+++ src/app/context/context.js
@@ -6,45 +6,45 @@ const AuthContext = createContext([]);
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
-=    const [loading, setLoading] = useState(true);
-const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([]);
 
 
-// Handle Login Get Data
-const fetchData = useCallback(async () => {
-    // fetch User data
-    const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
-    try {
-        setLoading(true)
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_SOME_URL}/api/user`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        setUser(response.data);
-        setLoading(false)
-    } catch (error) {
-        console.error(error);
-        setLoading(false)
-    } finally {
-        setLoading(false)
-    }
-}, []);
+    // Handle Login Get Data
+    const fetchData = useCallback(async () => {
+        // fetch User data
+        const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+        try {
+            setLoading(true)
+            const response = await axios.get(`${process.env.NEXT_PUBLIC_SOME_URL}/api/user`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            setUser(response.data);
+            setLoading(false)
+        } catch (error) {
+            console.error(error);
+            setLoading(false)
+        } finally {
+            setLoading(false)
+        }
+    }, []);
 
 
-// Handle Fetch User Data
-useEffect(() => {
-    fetchData()
-}, [fetchData])
+    // Handle Fetch User Data
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
 
 
 
 
-return (
-    <AuthContext.Provider value={{ user, loading, users, fetchData }}>
-        {children}
-    </AuthContext.Provider>
-);
+    return (
+        <AuthContext.Provider value={{ user, loading, users, fetchData }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 
